48776264b0e9245ee63aa49bb415595db59c893f
ltlx <ltlx4@icloud.com>
2022-02-01 21:15:01 +0100

    Define update_todo, delete_todo endpoints

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('add_todo/', views.add_todo, name='add-todo'),
+    path('delete_todo/<int:pk>', views.delete_todo, name='delete-todo'),
+    path('update_todo/<int:pk>', views.update_todo, name='update-todo'),
 ]
